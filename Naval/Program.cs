dc476ab415734836fc1b84fd7742de052f58e0a2
ZetaGG <zetag174@gmail.com>
2023-11-30 12:25:53 -0600

    Update Program.cs

--- Naval/Program.cs
+++ Naval/Program.cs
@@ -3,11 +3,14 @@ class Funciones
 {
     public void llenar(string[,] a, string[,] b, double r)
     {
-        
+        double b1 = r;
+        double b2 = r;
 
         for (int j = 0; j < r; j++)
         {
         Denuevo:
+            Console.Clear();
+            Console.WriteLine($"Barcos restantes {b1}");
             Console.WriteLine($"Jugador 1 ingrese la posicion su barco {j + 1}");
             Console.Write("X");
             int x = int.Parse(Console.ReadLine())-1;
@@ -23,13 +26,22 @@ class Funciones
                 Console.WriteLine("Ingresa numero dentro del tablero");
                 goto Denuevo;
             }
-
+            if (a[y,x]=="B")
+            {
+                Console.WriteLine("Esta posicion ya esta ocupada");
+                Console.ReadKey();
+                goto Denuevo;
+            }
             a[y, x] = "B";
+            b1--;
+
         }
 
         for (int j = 0; j < r; j++)
         {
         Denuevo2:
+            Console.Clear();
+            Console.WriteLine($"Barcos restantes {b2}");
             Console.WriteLine($"Jugador 2 ingrese la posicion su barco {j + 1}");
             Console.Write("X");
             int x = int.Parse(Console.ReadLine())-1;
@@ -46,7 +58,15 @@ class Funciones
                 goto Denuevo2;
             }
 
+            if (b[y, x] == "B")
+            {
+                Console.WriteLine("Esta posicion ya esta ocupada");
+                Console.ReadKey();
+                goto Denuevo2;
+            }
+
             b[y, x] = "B";
+            b2--;
         }
     }
 
@@ -99,16 +119,38 @@ class Funciones
                     Console.WriteLine("Tablero Jugador 2");
 
                     // Mostrar el tablero del jugador 2
+                VolverAtacar:
+                    Console.Clear();
                     for (int i = 0; i < N; i++)
                     {
                         for (int j = 0; j < N; j++)
                         {
-                            Console.Write(tab2[i, j] + "\t");
+                            if (tab2[i, j] == "A")
+                            {
+                                Console.ForegroundColor = ConsoleColor.DarkRed;
+                                Console.Write(tab2[i, j] + "\t");
+                                Console.ForegroundColor = ConsoleColor.White;
+
+                            }
+                            else if (tab2[i, j] == "V")
+                            {
+                                Console.ForegroundColor = ConsoleColor.Gray;
+                                Console.Write(tab2[i, j] + "\t");
+                                Console.ForegroundColor = ConsoleColor.White;
+
+                            }
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.Cyan;
+                                Console.Write(tab2[i, j] + "\t");
+                                Console.ForegroundColor = ConsoleColor.White;
+                            }
                         }
                         Console.WriteLine();
                     }
 
                     // Ingresar el ataque del jugador 1
+
                     Console.WriteLine("Jugador 1 Ingrese su ataque");
                     Console.Write("X: ");
                     int x = int.Parse(Console.ReadLine()) - 1;
@@ -129,8 +171,20 @@ class Funciones
                         Console.ReadKey();
                         continue;  // Vuelve al inicio del bucle
                     }
-                    tab2[y, x] = "B";
 
+                    if (tab2[y, x] == "A")
+                    {
+                        Console.WriteLine("Esta posicion ya a sido atacada");
+                        Console.ReadKey();
+                        goto VolverAtacar;
+                    }
+                    else if (tab2[y, x] == "V")
+                    {
+                        Console.WriteLine("Esta ya a sido atacada y esta vacia");
+                        Console.ReadKey();
+                        goto VolverAtacar;
+                    }
+                    tab2[y, x] = "B";
                     if (tab2[y, x] == b[y, x])
                     {
                         Console.WriteLine("Acertaste a un barco");
@@ -155,16 +209,39 @@ class Funciones
 
                     jugador = true;
                     break;
+                    //Fin del ataque del jugador 1
+
+
                 case true:
                 
+                    DenuevoAtaque2:
                     Console.Clear();
                     Console.WriteLine("Tablero Jugador 1");
                     for (int i = 0; i < N; i++)
                     {
                         for (int j = 0; j < N; j++)
                         {
+                            //Comparar el lugar y cambiar l color
+                            if (tab[i, j] == "A")
+                            {
+                                Console.ForegroundColor = ConsoleColor.DarkRed;
+                                Console.Write(tab[i, j] + "\t");
+                                Console.ForegroundColor= ConsoleColor.White;
 
-                            Console.Write(tab[i, j] + "\t");
+                            }
+                            else if (tab[i, j] == "V")
+                            {
+                                Console.ForegroundColor = ConsoleColor.Gray;
+                                Console.Write(tab[i, j] + "\t");
+                                Console.ForegroundColor = ConsoleColor.White;
+
+                            }
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.Cyan;
+                                Console.Write(tab[i, j] + "\t");
+                                Console.ForegroundColor = ConsoleColor.White;
+                            }
                         }
 
                         Console.WriteLine();
@@ -173,13 +250,13 @@ class Funciones
 
 
 
-                    DenuevoAtaque2:
                     Console.WriteLine("Jugador 2 Ingrese su ataque");
                     Console.Write("X");
                     int x2 = int.Parse(Console.ReadLine())-1;
                     if (x2 > N||x2<0)
                     {
                         Console.WriteLine("Ingresa numero dentro del tablero");
+                        Console.ReadKey();
                         goto DenuevoAtaque2;
                     }
                     Console.Write("Y");
@@ -187,8 +264,24 @@ class Funciones
                     if (y2 <0|| y2> N)
                     {
                         Console.WriteLine("Ingresa numero dentro del tablero");
+                        Console.ReadKey();
                         goto DenuevoAtaque2;
                     }
+
+                    //Comparar si ya a sdo atacado la posicion
+                    if (tab[y2, x2] == "A")
+                    {
+                        Console.WriteLine("Esta posicion ya a sido atacada");
+                        Console.ReadKey();
+                        goto DenuevoAtaque2;
+                    }
+                    else if (tab[y2, x2] == "V")
+                    {
+                        Console.WriteLine("Esta ya a sido atacada y esta vacia");
+                        Console.ReadKey();
+                        goto DenuevoAtaque2;
+                    }
+
                     tab[y2, x2] = "B";
                     if (tab[y2, x2] == a[y2, x2])
                     {
@@ -240,19 +333,13 @@ V=Vacio");
         string[,] tablero = new string[N, N];
         string[,] tablero2 = new string[N, N];
 
-        double numeroOriginal = (3 * 3) * 0.2;
+        double numeroOriginal = (N * N) * 0.2;
         double numeroRedondeado = Math.Round(numeroOriginal, 0);
         
         funciones.llenar(tablero, tablero2,numeroRedondeado);
         Console.Clear();
         bool jugador = false;
         funciones.Juego(N, tablero, tablero2, numeroRedondeado, jugador);
-        
-
-
-
-
-
     }
 }
 
